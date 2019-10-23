2018beae4c2a77ca27bacb674c4cdd4fe13d9aa0
Quentin Gaultier <quentin.gaultier@gmail.com>
2019-10-23 16:20:57 -0300

    test fix build

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -4,7 +4,7 @@ redis_port = Rails.application.secrets.redis && Rails.application.secrets.redis[
 # The constant below will represent ONE connection, present globally in models, controllers, views etc for the instance. No need to do Redis.new everytime
 if Rails.env.production?
   # REDIS =   Redis.new(host: redis_host, port: redis_port.to_i)
-  REDIS = Redis.new(ENV['REDIS_URL'])
+  REDIS = Redis.new(url: ENV["REDIS_URL"])
 else
   REDIS = Redis.new(host: redis_host, port: redis_port.to_i)
 end
