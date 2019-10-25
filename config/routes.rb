919aff81c72ebffb76b270881790640017844ea8
Quentin Gaultier <quentin.gaultier@gmail.com>
2019-10-25 11:33:30 -0300

    fix last position api

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
 
+  scope path: '/waypoints', controller: :waypoints do
+    get 'last_position' => :last_position
+  end
+
   resources :waypoints do
     collection do
       get 'polyline'
     end
-    member do
-      get 'last_position'
-    end
-
-
   end
 
 
